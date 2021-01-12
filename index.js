783822c315d023504262cbccf0541118ac6408c7
arbi1994 <arbi.1994@hotmail.com>
2021-01-12 17:26:45 +1100

    slideshow added

--- index.js
+++ index.js
@@ -4,4 +4,34 @@
 //     console.log(`letter ${i} is at ${logo[i].getTotalLength()}`);
 // }
 
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+
+const mySwiper = new Swiper('.swiper-container', {
+    speed: 1000,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+    delay: 6000,
+    waitForTransition: true
+    },
+    preloadImages: false,
+    lazy: {
+        loadPrevNext: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+});
+
+
+
+
+
+
 
